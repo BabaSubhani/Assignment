d78db945da4c357b0028dfeb7d2fdabf5ab4b16f
Subhani <babasubhani2004@gmail.com>
2025-07-07 12:30:42 +0530

    Moved React app into client folder

--- server/server.js
+++ server/server.js
@@ -28,4 +28,4 @@ app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server running on port https://adaptnxt-backend-79vr.onrender.com`));
